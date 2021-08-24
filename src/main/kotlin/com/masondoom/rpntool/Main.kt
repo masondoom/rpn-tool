d4814242190347131254dcc2940de20a7f1efe88
mason <xiemason@gmail.com>
2021-08-23 21:21:13 -0700

    Test udpate

--- src/main/kotlin/com/masondoom/rpntool/Main.kt
+++ src/main/kotlin/com/masondoom/rpntool/Main.kt
@@ -5,6 +5,7 @@ import com.masondoom.rpntool.core.calculator.Calculator
 import com.masondoom.rpntool.core.utils.InputParser
 
 fun main() {
+
     val calculator = Calculator()
     val inputParser = InputParser()
     CommandLineHandler(calculator, inputParser).start()
